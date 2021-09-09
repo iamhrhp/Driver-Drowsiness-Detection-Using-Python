4a5c23f868d822d8ac1fc7f76563990bd81198ba
Parvej khan <51136789+iamhrhp@users.noreply.github.com>
2021-09-09 15:53:12 +0530

    Update driver_drowsiness.py

--- driver_drowsiness.py
+++ driver_drowsiness.py
@@ -1,44 +1,33 @@
 import cv2
-# Numpy for array related functions
 import numpy as np
-# Dlib for deep learning based Modules and face landmark detection
 import dlib
-# face_utils for basic operations of conversion
 from imutils import face_utils
 
-# Initializing the camera and taking the instance
 cap = cv2.VideoCapture(0)
-
-# Initializing the face detector and landmark detector
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# status marking for current state
 sleep = 0
 drowsy = 0
 active = 0
 status = ""
 color = (0, 0, 0)
 
-
 def compute(ptA, ptB):
     dist = np.linalg.norm(ptA - ptB)
     return dist
-
 
 def blinked(a, b, c, d, e, f):
     up = compute(b, d) + compute(c, e)
     down = compute(a, f)
     ratio = up / (2.0 * down)
 
-    # Checking if it is blinked
     if (ratio > 0.25):
         return 2
     elif (ratio > 0.21 and ratio <= 0.25):
         return 1
     else:
         return 0
-
 
 _, f = cap.read()
 face_frame = f.copy()
@@ -47,7 +36,6 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
-    # detected face in faces array
     for face in faces:
         x1 = face.left()
         y1 = face.top()
@@ -60,13 +48,11 @@ while True:
         landmarks = predictor(gray, face)
         landmarks = face_utils.shape_to_np(landmarks)
 
-        # The numbers are actually the landmarks which will show eye
         left_blink = blinked(landmarks[36], landmarks[37],
                              landmarks[38], landmarks[41], landmarks[40], landmarks[39])
         right_blink = blinked(landmarks[42], landmarks[43],
                               landmarks[44], landmarks[47], landmarks[46], landmarks[45])
 
-        # Now judge what to do for the eye blinks
         if (left_blink == 0 or right_blink == 0):
             sleep += 1
             drowsy = 0
@@ -74,7 +60,6 @@ while True:
             if (sleep > 6):
                 status = "Sleeping !!!"
                 color = (255, 51, 51)
-
         elif (left_blink == 1 or right_blink == 1):
             sleep = 0
             active = 0
@@ -82,7 +67,6 @@ while True:
             if (drowsy > 6):
                 status = "Drowsy !"
                 color = (102, 102, 255)
-
         else:
             drowsy = 0
             sleep = 0
@@ -92,11 +76,9 @@ while True:
                 color = (102, 255, 102)
 
         cv2.putText(frame, status, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 3)
-
         for n in range(0, 68):
             (x, y) = landmarks[n]
             cv2.circle(face_frame, (x, y), 1, (255, 255, 255), -1)
-
     cv2.imshow("Frame", frame)
     cv2.imshow("Result of detector", face_frame)
     key = cv2.waitKey(1)
